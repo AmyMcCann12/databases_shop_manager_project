5142e5738f47055e01fe00681f1a671b33ee8fec
AmyMcCann <amy.mccann.34@gmail.com>
2023-11-30 13:28:43 +0000

    Prepared project repository using starter project

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from lib.database_connection import DatabaseConnection
-from lib.book_repository import BookRepository
 
 
 # Connect to the database
@@ -9,10 +8,3 @@ connection.connect()
 # Seed with some seed data
 connection.seed("seeds/book_store.sql")
 
-# Retrieve all books
-book_repository = BookRepository(connection)
-books = book_repository.all()
-
-# List them out
-for book in books:
-    print(book)
